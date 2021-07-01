2fa025b7f32fa5128c5832f2490a89fd2fb0db22
Nikolai Poroshin <picknick91@gmail.com>
2021-07-01 17:53:04 +0300

    fuck it all. start over

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-statements = int(input())
+from collections import defaultdict
 
-for n in range(statements):
-    inpt = input().replace(":", "").split()
-    subclass = inpt.pop(0)
-    parents = []
-    for i in inpt:
-        existing_parents = globals()[i].__bases__
-        globals()[i] = type(i, existing_parents, {})
-        parents.append(globals()[i])
-    p = tuple(parents)
-    globals()[subclass] = type(subclass, p, {})
+MRO = defaultdict(set)
 
-queries = int(input())
+# TODO: defaultdict
+# set - possible operations
 
-for i in range(queries):
-    qr = input().split()
-    if issubclass(globals()[qr[1]], globals()[qr[0]]):
-        print('Yes')
-    else:
-        print('No')
+for key, descendants in ...:
+    MRO[key].update(descendants)
+
+
+def is_subclass(class_, of_):
+    return class_ == of_ or of_ in MRO[class_]
